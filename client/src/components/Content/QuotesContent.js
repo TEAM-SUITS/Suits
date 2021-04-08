c70a914d1c099bf53f3d240b01be114597dda1ca
Min Gee Suh <suh@student.unimelb.edu.au>
2021-04-08 14:10:36 +0900

    refactor: removed unnecessary p tag

--- client/src/components/Content/QuotesContent.js
+++ client/src/components/Content/QuotesContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { museoSmall } from "styles/common/common.styled";
 
 /* ---------------------------- styled component ---------------------------- */
 
@@ -7,19 +8,19 @@ const Quote = styled.q`
   quotes: none;
 `;
 const QuoteBy = styled.footer`
+  ${museoSmall}
   margin-top: 1em;
   text-align: right;
+  color: var(--color-gray3);
 `;
 
 export default function QuotesContent({ children, cite, author }) {
   return (
     <article>
-      <p>
-        <Quote cite={cite} lang="en">
-          {children}
-        </Quote>
-        <QuoteBy>{author}</QuoteBy>
-      </p>
+      <Quote cite={cite} lang="en">
+        {children}
+      </Quote>
+      <QuoteBy>{author}</QuoteBy>
     </article>
   );
 }
