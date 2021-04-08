b9def6446be48f5fba6688badbb5b6b81cc9218c
wisemett <frontendkid@gmail.com>
2021-04-08 19:28:16 +0900

    feat: Modify width to height in Tier component

--- client/src/components/Tier/Tier.stories.js
+++ client/src/components/Tier/Tier.stories.js
@@ -24,10 +24,10 @@ export default {
       description: '(1 ~ 6) 사용자 등급 설정',
       defaultValue: 1,
     },
-    width: {
+    height: {
       control: 'number',
-      description: '넥타이 핀 이미지 사이즈(width)',
-      defaultValue: 100,
+      description: '넥타이 핀 이미지 사이즈(height)',
+      defaultValue: 10,
     },
   },
 };
@@ -39,62 +39,62 @@ const Template = (args) => <Tier {...args} />;
 export const Tier1 = Template.bind({});
 Tier1.args = {
   tier: 1,
-  width: 100,
+  height: 15,
 };
 export const Tier1Small = Template.bind({});
 Tier1Small.args = {
   tier: 1,
-  width: 43,
+  height: 6,
 };
 export const Tier2 = Template.bind({});
 Tier2.args = {
   tier: 2,
-  width: 100,
+  height: 15,
 };
 export const Tier2Small = Template.bind({});
 Tier2Small.args = {
   tier: 2,
-  width: 43,
+  height: 6,
 };
 export const Tier3 = Template.bind({});
 Tier3.args = {
   tier: 3,
-  width: 100,
+  height: 15,
 };
 export const Tier3Small = Template.bind({});
 Tier3Small.args = {
   tier: 3,
-  width: 43,
+  height: 6,
 };
 export const Tier4 = Template.bind({});
 Tier4.args = {
   tier: 4,
-  width: 100,
+  height: 15,
 };
 export const Tier4Small = Template.bind({});
 Tier4Small.args = {
   tier: 4,
-  width: 43,
+  height: 6,
 };
 
 export const Tier5 = Template.bind({});
 Tier5.args = {
   tier: 5,
-  width: 100,
+  height: 15,
 };
 export const Tier5Small = Template.bind({});
 Tier5Small.args = {
   tier: 5,
-  width: 43,
+  height: 6,
 };
 
 export const Tier6 = Template.bind({});
 Tier6.args = {
   tier: 6,
-  width: 100,
+  height: 15,
 };
 export const Tier6Small = Template.bind({});
 Tier6Small.args = {
   tier: 6,
-  width: 43,
+  height: 6,
 };
