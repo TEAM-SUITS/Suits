1da21516eb9908e90e7cfd22e5fd7c70d88a479f
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-04-14 13:27:56 +0900

    fix: answer model 일부 수정

--- server/model/Answer.js
+++ server/model/Answer.js
@@ -5,7 +5,7 @@ const answerSchema = new Schema({
   content: String,
   postedby: { type: Schema.Types.ObjectId, ref: "User" },
   postedOn: { type: Date, default: Date.now },
-  question: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  question: { type: Schema.Types.ObjectId, ref: "Question" },
   likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
