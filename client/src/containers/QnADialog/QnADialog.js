4d38418cc034957a3d102e62bf69107c101a7ea1
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-04-21 19:23:38 +0900

    refactor: qna dialog 수정
    - 임시 데이터 구조 기반으로 작성되었던 기존 코드를,
    현재의 데이터 구조에 맞게 변경
    - 사용자의 답변 작성 여부에 따라 text area 렌더링 여부를
    결정하는 로직 추가(하나의 질문에는 각 사용자 당 한 번씩만
    답변 가능)

---
+++ client/src/containers/QnADialog/QnADialog.js
@@ -0,0 +1,149 @@
+import Portal from 'components/Portal/Portal';
+import Dialog from 'components/Dialog/Dialog';
+import Card from 'components/Card/Card';
+import QnAContent from 'components/Content/QnAContent';
+import Divider from 'components/Divider/Divider';
+import Hashtag from 'components/Hashtag/Hashtag';
+import styled from 'styled-components';
+import {
+  boxShadowBlack,
+  spoqaMedium,
+  spoqaMediumLight,
+} from 'styles/common/common.styled';
+import { bool, object } from 'prop-types';
+import API from 'api/api';
+
+/* ---------------------------- styled components --------------------------- */
+const CardContainer = styled.div`
+  > div {
+    background-color: transparent;
+    margin-top: 14px;
+    box-shadow: none;
+  }
+`;
+
+const HashtagContainer = styled.div`
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+  justify-content: space-around;
+  margin: 2em auto;
+`;
+
+const AnswerContainer = styled.div`
+  margin: 3em auto 1em;
+  position: relative;
+  max-width: 347px;
+
+  textarea {
+    ${spoqaMediumLight}
+    background-color: var(--color-lightgray2);
+    padding: 1em;
+    border: solid 1px var(--color-gray1);
+    border-radius: 5px;
+    width: 100%;
+    height: 10em;
+    resize: none;
+    ${boxShadowBlack}
+  }
+
+  button {
+    background-color: var(--color-gray3);
+    color: var(--color-lightgray1);
+    border: none;
+    border-radius: 5px;
+    width: 60px;
+    ${boxShadowBlack}
+    ${spoqaMedium}
+    padding: 0 3px;
+    position: absolute;
+    bottom: 0.6em;
+    right: 0.3em;
+    cursor: pointer;
+  }
+`;
+
+/* ------------------------------- 답변 영역 분기 처리 ------------------------------ */
+const Answers = (answersList = []) => {
+  if (!answersList.length) {
+    return (
+      <QnAContent
+        answer={false}
+        isEllipsis={false}
+      />
+    )
+  }
+
+  return (
+    answersList.map((answer) => {
+      return (
+        <>
+          <QnAContent
+            key={answer._id}
+            answer={answer}
+            isEllipsis={false}
+          />
+          <Divider
+            primary={false}
+            color="gray"
+            height="1px"
+            width="50%"
+          />
+        </>
+      );
+    })
+  );
+};
+
+const InputArea = answered => {
+  if (answered) return null;
+
+  return (
+    <AnswerContainer>
+      <textarea />
+      <button>Post</button>
+    </AnswerContainer>
+  );
+};
+
+const getIsAnswered = async questionId => {
+  const userData = await API('/api/user-profile', 'get');
+  let isAnswered = false;
+
+  userData[0].answeredQuestions.forEach(({ _id }) => {
+    if (_id === questionId) isAnswered = true;
+  });
+
+  return isAnswered;
+};
+
+/* ------------------------------- QnA Dialog ------------------------------- */
+export default function QnADialog({ isVisible, question = {} }) {
+  return (
+    <Portal id={'dialog-container'}>
+      <Dialog
+        visible={isVisible} // 상위 컴포넌트의 state로 handle
+        label="QnA 상세 내용"
+      >
+        <CardContainer>
+        <Card isQuestion title={question.content}>
+          <HashtagContainer>
+            {question.hashTag.map((keyword, idx) => {
+              return <Hashtag key={idx} type={keyword} />
+            })}
+          </HashtagContainer>
+          <Answers answersList={question.answers} />
+          <InputArea answered={getIsAnswered(question._id)} />
+        </Card>
+        </CardContainer>
+      </Dialog>
+    </Portal>
+  );
+}
+
+/* ------------------------------- prop types ------------------------------- */
+
+QnADialog.propTypes = {
+  isVisible: bool,
+  question: object,
+};
