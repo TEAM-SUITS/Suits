61f5dee92733420ad2b553fc4f9914e5b24222cf
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-04-22 12:28:11 +0900

    style: 팔로잉페이지 이름 수정(기존: LikedPage, 변경 후: FollowingPage)

--- client/src/pages/FollowingPage/FollowingPage.js
+++ client/src/pages/FollowingPage/FollowingPage.js
@@ -3,14 +3,19 @@ import styled from "styled-components";
 import PageContainer from "containers/PageContainer/PageContainer.styled";
 import { pageEffect } from "styles/motions/variants";
 import TextHeaderBar from "containers/TextHeaderBar/TextHeaderBar";
+import Hashtag from "components/Hashtag/Hashtag";
 
 /* -------------------------------------------------------------------------- */
 
-export default function LikedPage() {
+export default function FollowingPage() {
   return (
     <>
       <TextHeaderBar page="liked" />
       <PageContainer variants={pageEffect} initial="hidden" animate="visible">
+        <Hashtag
+          type='All'
+          isSelected={false}
+        />
       </PageContainer>
     </>
   );
