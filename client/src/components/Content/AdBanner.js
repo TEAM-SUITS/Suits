6a3f80975195842b87198f3300ed93572bf84716
wisemett <frontendkid@gmail.com>
2021-04-27 01:53:14 +0900

    feat: Add Images for Ad Banner, Remove Dots of Carousel

--- client/src/components/Content/AdBanner.js
+++ client/src/components/Content/AdBanner.js
@@ -3,8 +3,11 @@ import 'slick-carousel/slick/slick-theme.css';
 import React from 'react';
 import Slider from 'react-slick';
 import styled from 'styled-components';
+import { boxShadow } from 'styles/common/common.styled';
 
 const SliderContainer = styled.div`
+  background-color: var(--color-white);
+  ${boxShadow};
   width: 100%;
 `;
 const StyledSlider = styled(Slider)`
@@ -21,29 +24,29 @@ const StyledSlider = styled(Slider)`
 const adList = [
   {
     name: '데브폴리오',
-    src: '/assets/adbanner1.png',
+    src: '/assets/ad-devfolio.png',
     url: 'https://github.com/Devfolio-team/Devfolio-Client',
   },
   {
     name: '산타',
-    src: '/assets/adbanner2.png',
+    src: '/assets/ad-santa.png',
     url: 'https://github.com/Santa-Application/App',
   },
   {
     name: '우연히, 식단',
-    src: '/assets/adbanner3.png',
+    src: '/assets/ad-diet.png',
     url: 'https://github.com/hyorard-b/forte-diet',
   },
   {
     name: '살롱',
-    src: '/assets/adbanner1.png',
+    src: '/assets/ad-salon.png',
     url: 'https://github.com/riding-bom/salon',
   },
-  {
-    name: 'LVPS',
-    src: '/assets/adbanner2.png',
-    url: 'https://github.com/FDS-18-Final-Project',
-  },
+  // {
+  //   name: 'LVPS',
+  //   src: '/assets/.png',
+  //   url: 'https://github.com/FDS-18-Final-Project',
+  // },
 ];
 
 /* --------------------------------- 광고주 리스트 -------------------------------- */
@@ -64,13 +67,13 @@ export default function AdBanner() {
 
   // 캐러셀 설정
   const settings = {
-    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
+    autoplaySpeed: 4000,
   };
 
   return (
